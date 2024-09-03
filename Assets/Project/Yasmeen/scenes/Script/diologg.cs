906cb9d95f70ce15dfffc45d3b754ccf487ffad8
yasmeen962007 <yasmeenyaseer92@gmail.com>
2024-09-03 19:32:21 +0300

    ui

--- Assets/Project/Yasmeen/scenes/Script/diologg.cs
+++ Assets/Project/Yasmeen/scenes/Script/diologg.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 
 public class diologg : MonoBehaviour
 {
-    [SerializeField] Flowchart Flowchart;
-    [SerializeField] string blockname;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("player"))
-        {
-            Flowchart.ExecuteBlock(blockname); // تنفيذ الحوار عند دخول اللاعب المنطقة
-        }
-    }
+    //[SerializeField] Flowchart flow;
+    //[SerializeField] string Exblockname;
+    //[SerializeField] string StopBlockName;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        flow.ExecuteBlock(Exblockname); // تنفيذ الحوار عند دخول اللاعب المنطقة
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("player"))
-        {
-            Flowchart.StopBlock(blockname); // إيقاف الحوار عند خروج اللاعب من المنطقة
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //       
+    //        if(StopBlockName != null)
+    //        {
+    //            Debug.Log("not null");
+
+    //            flow.ExecuteBlock(StopBlockName); // إيقاف الحوار عند خروج اللاعب من المنطقة
+    //        }
+    //    }
+    //}
 }
