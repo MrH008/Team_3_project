c87dd03a3add1621a2effa5ec1a782f7d9901434
Malak4m <mstfythmanmlk@gmail.com>
2024-09-10 11:52:51 +0300

    desk

---
+++ Assets/Project/Malak/Scripts/DeskTrigger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject monitor;
+    [SerializeField] GameObject printer;
+    [SerializeField] GameObject projector;
+    [SerializeField] GameObject keybosrd;
+    [SerializeField] GameObject case2 ;
+    [SerializeField] GameObject laptop;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Monitor"))
+        {
+            monitor.SetActive(true);
+            Destroy(other);
+        }
+        else if (other.CompareTag("printer"))
+        {
+            printer.SetActive(true);
+            Destroy(other);
+        }
+        else if (other.CompareTag("projector"))
+        {
+            projector.SetActive(true);
+            Destroy(other);
+        }
+        else if (other.CompareTag("keybosrd"))
+        {
+            keybosrd.SetActive(true);
+            Destroy(other);
+        }
+        else if (other.CompareTag("case2"))
+        {
+            case2.SetActive(true);
+            Destroy(other);
+        }
+        else if (other.CompareTag("laptop"))
+        {
+            laptop.SetActive(true);
+            Destroy(other);
+        }
+
+
+
+
+    }
+}
