6007b689dc0448219c85deb5d4b08b3341f7cd86
Malak4m <mstfythmanmlk@gmail.com>
2024-09-13 19:49:12 +0300

    score

--- Assets/Project/Malak/Scripts/DeskTrigger.cs
+++ Assets/Project/Malak/Scripts/DeskTrigger.cs
@@ -1,14 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;  
 
 public class DeskTrigger : MonoBehaviour
 {
     [SerializeField] GameObject monitor;
     [SerializeField] GameObject printer;
     [SerializeField] GameObject projector;
-    [SerializeField] GameObject keybosrd;
-    [SerializeField] GameObject case2 ;
+    [SerializeField] GameObject keyboard;
+    [SerializeField] GameObject case2;
+
+    [SerializeField] TMP_Text scoreText;  
+    private int score = 0;  
+
+    private void Start()
+    {
+        UpdateScoreText();  
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,34 +25,48 @@ public class DeskTrigger : MonoBehaviour
         {
             monitor.SetActive(true);
             Destroy(other.gameObject);
-
+            IncreaseScore();
         }
         else if (other.CompareTag("printer"))
         {
             printer.SetActive(true);
             Destroy(other.gameObject);
-
+            IncreaseScore();
         }
         else if (other.CompareTag("projector"))
         {
             projector.SetActive(true);
             Destroy(other.gameObject);
-
+            IncreaseScore();
         }
         else if (other.CompareTag("keybosrd"))
         {
-            keybosrd.SetActive(true);
+            keyboard.SetActive(true);
             Destroy(other.gameObject);
-
+            IncreaseScore();
         }
         else if (other.CompareTag("case2"))
         {
             case2.SetActive(true);
             Destroy(other.gameObject);
-
+            IncreaseScore();
         }
-     
+    }
 
+    
+    private void IncreaseScore()
+    {
+        score += 1;
+        UpdateScoreText(); 
+    }
 
+    
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score.ToString();
+        }
     }
 }
+
