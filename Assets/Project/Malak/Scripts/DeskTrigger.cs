619b2c920e97d9fd1e56ca9d1890caed4f24bae7
Malak4m <mstfythmanmlk@gmail.com>
2024-09-13 18:42:32 +0300

    update

--- Assets/Project/Malak/Scripts/DeskTrigger.cs
+++ Assets/Project/Malak/Scripts/DeskTrigger.cs
@@ -9,7 +9,7 @@ public class DeskTrigger : MonoBehaviour
     [SerializeField] GameObject projector;
     [SerializeField] GameObject keybosrd;
     [SerializeField] GameObject case2 ;
-    [SerializeField] GameObject laptop;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("monitor"))
@@ -42,11 +42,7 @@ public class DeskTrigger : MonoBehaviour
             Destroy(other.gameObject);
 
         }
-        else if (other.CompareTag("laptop"))
-        {
-            laptop.SetActive(true);
-            Destroy(other.gameObject);
-        }
+     
 
 
     }
